606ec7ba786e07716e1fdf6ce8cfc65afc910080
Akash <akashguptayt@gmail.com>
2024-06-15 00:04:23 +0530

    removed about page with route, created a basic form to start

--- first_app/first_app.py
+++ first_app/first_app.py
@@ -12,22 +12,50 @@ class State(rx.State):
     ...
 
 
+class FormState(rx.State):
+    form_data: dict = {}
+
+    def handle_submit(self, form_data: dict):
+        """Handle the form submit."""
+        self.form_data = form_data
+
+
+def form_example():
+    return rx.center(
+        rx.vstack(
+        rx.form(
+            rx.vstack(
+                rx.input(
+                    placeholder="First Name",
+                    name="first_name",
+                ),
+                rx.input(
+                    placeholder="Last Name",
+                    name="last_name",
+                ),
+                rx.hstack(
+                    rx.checkbox("Checked", name="check"),
+                    rx.switch("Switched", name="switch"),
+                ),
+                rx.button("Submit", type="submit"),
+            ),
+            on_submit=FormState.handle_submit,
+            reset_on_submit=True,
+        ),
+        rx.divider(),
+        rx.heading("Results"),
+        rx.text(FormState.form_data.to_string()),
+    )
+    ) 
+    
+
 def index() -> rx.Component:
     # Welcome Page (Index)
     return rx.container(
-        navbar(),
-
+        # navbar(),
+        form_example(),
     )
-
-def about():
-    # About Page
-    return rx.container(
-        rx.text('About Page')
-    )
-
-
 
 app = rx.App()
 app.add_page(index)
-app.add_page(about, route='/about')
 app.add_page(navbar)
