3f0e34ed1bd671ed5d9bc471cd3647c09e933fa6
Akash <akashguptayt@gmail.com>
2024-06-17 19:57:24 +0530

    Removed the sample form from the index page

--- first_app/first_app.py
+++ first_app/first_app.py
@@ -4,58 +4,22 @@ import reflex as rx
 
 from rxconfig import config
 from .components.navbar import navbar
-
-
-class State(rx.State):
-    """The app state."""
-
-    ...
-
-
-class FormState(rx.State):
-    form_data: dict = {}
-
-    def handle_submit(self, form_data: dict):
-        """Handle the form submit."""
-        self.form_data = form_data
-
-
-def form_example():
-    return rx.center(
-        rx.vstack(
-        rx.form(
-            rx.vstack(
-                rx.input(
-                    placeholder="First Name",
-                    name="first_name",
-                ),
-                rx.input(
-                    placeholder="Last Name",
-                    name="last_name",
-                ),
-                rx.hstack(
-                    rx.checkbox("Checked", name="check"),
-                    rx.switch("Switched", name="switch"),
-                ),
-                rx.button("Submit", type="submit"),
-            ),
-            on_submit=FormState.handle_submit,
-            reset_on_submit=True,
-        ),
-        rx.divider(),
-        rx.heading("Results"),
-        rx.text(FormState.form_data.to_string()),
-    )
-    ) 
-    
+from .views.header.header import header
+from .components.form_field import form_field
+from .backend.backend import State
+from .views.main import add_collection
 
 def index() -> rx.Component:
     # Welcome Page (Index)
-    return rx.container(
-        # navbar(),
-        form_example(),
+    return rx.vstack(
+        navbar(),
+        header(),
+        rx.box(
+            add_collection(),
+        ),  
     )
 
 app = rx.App()
 app.add_page(index)
 app.add_page(navbar)
+app._compile() # Compile the app 
