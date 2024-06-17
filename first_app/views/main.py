e48201676b9cc9bc4a46406eed555dfc45570e02
Akash <akashguptayt@gmail.com>
2024-06-17 19:56:11 +0530

    main file to handle view in the index page

---
+++ first_app/views/main.py
@@ -0,0 +1,85 @@
+import reflex as rx
+from ..backend.backend import State
+from ..components.form_field import form_field
+  
+def add_collection() -> rx.Component:
+    return rx.dialog.root(
+        rx.dialog.trigger(
+            rx.button(
+                rx.icon("plus", size=26),
+                rx.text("Add Collection", size="4",
+                        ), color="primary"
+                ),
+        ),
+        rx.dialog.content(
+            rx.hstack(
+                rx.badge(
+                    rx.icon(tag="folder", size=34),
+                    color_scheme="grass",
+                    radius="full",
+                    padding="0.65rem",
+                ),
+                rx.vstack(
+                    rx.dialog.title("Add Collection",
+                        weight="bold",
+                        margin="0",
+                    ),
+                    rx.dialog.description(
+                        "fill the collection name",
+                    ),
+                    spacing="1",
+                    height="100%",
+                    align_items="start",
+                ),
+                height="100%",
+                spacing="4",
+                margin_bottom="1.5em",
+                align_items="center",
+                width="100%",
+            ),
+            rx.flex(
+                rx.form.root(
+                    rx.flex(
+                        #Name
+                        form_field(
+                            "Name",
+                            "Collection Name",
+                            "text",
+                            "name",
+                            "folder",
+                            
+                        ),
+                    ),
+                    rx.flex(
+                        rx.dialog.close(
+                            rx.button(
+                                "Cancel",
+                                variant="soft",
+                                color_scheme="gray",
+                            ),
+                        ),
+                        rx.form.submit(
+                            rx.dialog.close(
+                                rx.button("Add Collection", color="primary"),
+                            ),
+                            as_child=True,
+                        ),
+                        padding_top="2em",
+                        spacing="3",
+                        mt="4",
+                        justify="end",
+                    ),
+                    on_submit=State.add_collection_to_db,
+                    reset_on_submit=False,
+                ),
+                width="100%",
+                direction="column",
+                spacing="4",
+            ),
+            style={"max_width": 450},
+            box_shadow="lg",
+            padding="1.5em",
+            border=f"2px solid {rx.color('accent', 7)}",
+            border_radius="25px",
+        ),
+    )
