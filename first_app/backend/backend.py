bb31767aa70e39a36f22daf4e70af897e95c84b2
Akash <akashguptayt@gmail.com>
2024-06-19 11:29:36 +0530

    updated the code to use rx.Model instead of SQLModel and also added a new Workspace class, established relatonship between two

--- first_app/backend/backend.py
+++ first_app/backend/backend.py
@@ -1,25 +1,29 @@
 import reflex as rx
-from typing import Optional
-from sqlmodel import Field, SQLModel, create_engine, Session, select
+from typing import Optional, List
+from sqlmodel import Field, SQLModel, create_engine, Session, select, Relationship
 
-class Collection(SQLModel, table=True):
+class Workspace(rx.Model, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
+    collections: List["Collection"] = Relationship(back_populates="workspace")
+
+class Collection(rx.Model, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    
+    workspace_id: int = Field(foreign_key="workspace.id")
+    workspace: Optional[Workspace] = Relationship(back_populates="collections")
+    
 
 engine = create_engine("sqlite:///database.db", echo=True) # echo=True will print all SQL queries to the terminal
-# SQLModel.metadata.create_all(engine)    # Create the database schema for all the SQLModel classes
+SQLModel.metadata.create_all(engine)    # Create the database schema for all the SQLModel classes
 
 class State(rx.State):
     
     def add_collection_to_db(self, form_data: dict):
-        collection = Collection(name=form_data["name"])
-        with Session(engine) as session:
+        collection = Collection(name=form_data["name"],workspace_id=form_data["workspace_id"])
+        with rx.session() as session:
             session.add(collection)
             session.commit()
         return rx.toast.info(f"Collection {form_data["name"]} has been added.", variant="outline", position="bottom-right")
-    
-    def create_db_and_table(self):
-        SQLModel.metadata.create_all(engine)
-        
-    if __name__ == "__main__":
-        State.create_db_and_table()
+ 
